d6b86a1bd403109d3fee4ded1c9f2d20f7b0befb
Mohammed Yaseen <mohammedysn15@gmail.com>
2020-06-15 11:12:49 +0100

    Create simulator class

--- app.py
+++ app.py
@@ -1,5 +1,27 @@
+import characters
+import random
+
+
+class Simulator:
+    def __init__(self):
+        self.classes = [characters.Barbarian, characters.Elf,
+                        characters.Wizard, characters.Dragon, characters.Knight]
+        self.character_set = [self.generate_characters() for i in range(10)]
+        self.display_character_set()
+
+    def generate_characters(self):
+        return random.choice(self.classes)()
+
+    def display_character_set(self):
+        for character in self.character_set:
+            character.display_stats()
+
+    def show_menu(self):
+        pass
+
+
 def main():
-    pass
+    Simulator()
 
 
 if __name__ == "__main__":
