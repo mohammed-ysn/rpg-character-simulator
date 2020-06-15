4eb608cda53d31a2fb5453423936e7e13ce94ab4
Mohammed Yaseen <mohammedysn15@gmail.com>
2020-06-15 11:27:03 +0100

    Create menu

--- app.py
+++ app.py
@@ -7,7 +7,9 @@ class Simulator:
         self.classes = [characters.Barbarian, characters.Elf,
                         characters.Wizard, characters.Dragon, characters.Knight]
         self.character_set = [self.generate_characters() for i in range(10)]
-        self.display_character_set()
+        self.show_menu_again = True
+        while self.show_menu_again:
+            self.main_menu()
 
     def generate_characters(self):
         return random.choice(self.classes)()
@@ -16,8 +18,28 @@ class Simulator:
         for character in self.character_set:
             character.display_stats()
 
-    def show_menu(self):
-        pass
+    def main_menu(self):
+        try:
+            print('-' * 20)
+            print('Menu')
+            print('-' * 20)
+            print('1) Display characters')
+            print('2) Edit characters')
+            print('3) Export characters')
+            print('4) End program\n')
+            option = int(input('Option (1-4): '))
+            if option == 1:
+                print('In progress')
+            elif option == 2:
+                print('In progress')
+            elif option == 3:
+                print('In progress')
+            elif option == 4:
+                self.show_menu_again = False
+            else:
+                raise ValueError
+        except ValueError:
+            print('You entered an invalid option.\n')
 
 
 def main():
