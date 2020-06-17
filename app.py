48eebc0360896caab90340c47681b7540bede6de
Mohammed Yaseen <mohammedysn15@gmail.com>
2020-06-17 17:02:54 +0100

    Split menu

--- app.py
+++ app.py
@@ -14,15 +14,18 @@ class Simulator:
     def generate_characters(self):
         return random.choice(self.classes)()
 
+    def display_menu(self):
+        print('\n' + '-' * 20)
+        print('Menu')
+        print('-' * 20)
+        print('1) Display characters')
+        print('2) Edit characters')
+        print('3) Export characters')
+        print('4) End program')
+
     def main_menu(self):
         try:
-            print('\n' + '-' * 20)
-            print('Menu')
-            print('-' * 20)
-            print('1) Display characters')
-            print('2) Edit characters')
-            print('3) Export characters')
-            print('4) End program')
+            self.display_menu()
             option = int(input('\nOption (1-4): '))
             if option == 1:
                 for character in self.character_set:
@@ -36,6 +39,12 @@ class Simulator:
                     raise ValueError
                 character = self.character_set[option - 1]
                 character.display_stats()
+                attribute = {'type': 'character_type',
+                             'health': 'health',
+                             'power': 'power',
+                             'special attack power': 'sp_attack_power',
+                             'speed': 'speed'}[input('\nAttribute to edit: ').lower()]
+                setattr(character, attribute, input('New value: '))
             elif option == 3:
                 print('In progress')
             elif option == 4:
@@ -43,7 +52,9 @@ class Simulator:
             else:
                 raise ValueError
         except ValueError:
-            print('You entered an invalid option.')
+            print('\nYou entered an invalid option.')
+        except KeyError:
+            print('\nYou entered an invalid attribute.')
 
 
 def main():
